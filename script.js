7cd16f9e19ec2af6bf471655aff4c755d5c2ade7
Antonio Gagliardo <ajgagliardo92@gmail.com>
2023-11-02 22:17:16 -0700

    gave names to the choices to rock, paper, scissors instead of just numbers

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log('Hello World!')
+
+function computerChoice(){
+    n = Math.floor(Math.random()*2);
+    let AIChoice;
+
+    if (n == 0){
+        AIChoice = 'rock';
+    } else if (n == 1){
+        AIChoice= 'paper';
+    } else{
+        AIChoice == 'scissors';}
+    return AIChoice;
+}
+
+
+// computerChoice();
+
+
+
+
+/* 
+Logic should be:
+Player writes a prompt
+check if prompt is correct
+if correct, keep, if not, repeat.
+*/
+
+// function playerSelection((prompt("write rock, paper or scissors").toLowerCase)){
+//     return playerSelection;
+// }
