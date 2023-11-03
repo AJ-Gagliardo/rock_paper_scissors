850317c8f72029fa4fcde7a5a7670dd9ab2378dd
Antonio Gagliardo <ajgagliardo92@gmail.com>
2023-11-03 13:57:50 -0700

    joined the function so player can choose and the play round, now as long as user selects rock, it is working perfectly

--- script.js
+++ script.js
@@ -35,10 +35,12 @@ if correct, keep, if not, repeat.
 
 
 
-// function playerSelection(){
-//     let playerChoice = prompt("write rock, paper or scissors").toLowerCase;
-//     return playerChoice;
-// }
+function playerChoice(){
+    let playersPrompt = prompt("write rock, paper or scissors").toLowerCase();
+    return playersPrompt;
+        // console.log(playersPrompt)
+    
+}
 
 /*
 // this is wokring but im trying stuff with it
@@ -52,20 +54,30 @@ winMessage = `You win ${playerSelection} beats ${computerSelection}`;
 
 
 // trying in another format
-function playRound(playerSelection = 'rock', computerSelection= 'scissors'){
+function playRound(playerSelection, computerSelection= 'scissors'){
+    playerSelection = playerChoice();
     winMessage = `You win ${playerSelection} beats ${computerSelection}`;
-    loseMessage = `You lose ${computerSelection} beats ${playerSelection}`
-    tieMessage = `Tie, you both chose ${playerSelection}`
+    loseMessage = `You lose ${computerSelection} beats ${playerSelection}`;
+    tieMessage = `Tie, you both chose ${playerSelection}`;
+    naMessage = `${playerSelection} is not a valid option, please try again`;
         if ((playerSelection == 'rock') && (computerSelection == 'scissors')){
             return winMessage;
-
+    }
+        else if ((playerSelection == 'rock') && (computerSelection == 'paper')){
+            return loseMessage;
+        }
+        else if ((playerSelection == 'rock') && (computerSelection == 'rock')){
+            return tieMessage;
+        }
+        else { return naMessage};
     }
 
 
 
 
+// const playerSelection = 'rock';
+// const computerSelection = 'scissors'
 
-const playerSelection = 'rock';
-const computerSelection = 'scissors'
+
 // const computerSelection = getComputerChoice();
 // console.log(PlayRound(playerSelection,computerSelection));
