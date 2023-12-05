4bcad07508244c236f4f336485290a76503f3b10
Antonio Gagliardo <ajgagliardo92@gmail.com>
2023-12-04 19:34:16 -0800

    finished the game with UI

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+// Computer random choice
 function getComputerChoice(){
     n = Math.floor(Math.random()*3);
     let AIChoice;
@@ -11,11 +12,7 @@ function getComputerChoice(){
     return AIChoice;
 }
 
-const results = document.querySelector('.results');
-// results.textContent = 'asd2'
-console.log(results);
-const image = document.querySelector('.image');
-console.log(image)
+
 
 
 
@@ -30,22 +27,34 @@ function playRound(playerSelection, computerSelection){
     (playerSelection == 'paper') && (computerSelection == 'rock')||
     (playerSelection == 'scissors')&&(computerSelection == 'paper')){
         results.textContent= winMessage;
+        playerScore +=1
+        playerScoreQuery.textContent = playerScore;
         image.src="img/win.jpg";
-        return winMessage;
+        // return winMessage;
+        if (playerScore ===  5){
+            alert('You won!');
+            resetGame();
+        }
+        else return;
         
     }
     else if (((playerSelection == 'rock') && (computerSelection == 'paper')) || 
     (playerSelection == 'paper') && (computerSelection == 'scissors') ||
     (playerSelection == 'scissors')&& (computerSelection == 'rock')){
         results.textContent= loseMessage;
+        aiScore +=1;
+        aiScoreQuery.textContent = aiScore
         image.src="img/lose.jpg";
-        return loseMessage;
+        // return loseMessage;
+        if(aiScore ===5){
+            alert("AI won :( this is the beginning of machine's revolution")
+        }
 
     }
     else if ((playerSelection == computerSelection)){
         results.textContent= tieMessage;
         image.src="img/tie.jpg";
-        return tieMessage;
+        // return tieMessage;
     }
     else { return naMessage};
 }
@@ -57,93 +66,47 @@ function playRound(playerSelection, computerSelection){
 const rock = document.querySelector('.rock-btn');
 rock.addEventListener("click", function(){
     const result = playRound('rock', getComputerChoice());
-    console.log(result);
+
 });
 
 
 const paper = document.querySelector('.paper-btn');
 paper.addEventListener("click",function(){
     const result = playRound('paper', getComputerChoice());
-    console.log(result);
+
 })
 
 const scissors = document.querySelector('.scissors-btn');
 scissors.addEventListener("click", function(){
     const result = playRound('scissors', getComputerChoice());
-    console.log(result);
+
 
 })
 
+// UI results
+
+const results = document.querySelector('.results');
+console.log(results);
+const image = document.querySelector('.image');
+console.log(image)
+
+const playerScoreQuery = document.querySelector('.player-score');
+console.log(playerScoreQuery);
+let playerScore = 0;
+playerScoreQuery.textContent = playerScore;
+
+let aiScore = 0;
+const aiScoreQuery = document.querySelector('.ai-score');
+aiScoreQuery.textContent = aiScore;
 
 
-// trying in another format
+// reset function
+function resetGame(){
+    playerScore = 0;
+    aiScore = 0;
+    playerScoreQuery.textContent = playerScore;
+    aiScoreQuery.textContent = aiScore;
 
-/* // Old function
 
-
-function playerChoice(){
-    let playersPrompt = prompt("write rock, paper or scissors").toLowerCase();
-    return playersPrompt;
-   
-    
 }
 
-function playRound(playerSelection, computerSelection){
-    playerSelection = playerChoice();
-    computerSelection = getComputerChoice();
-    winMessage = `You win, you chose ${playerSelection} which beats AI's ${computerSelection}`;
-    loseMessage = `You lose, AI chose ${computerSelection} which beats your ${playerSelection}`;
-    tieMessage = `Tie, you both chose ${playerSelection}`;
-    naMessage = `${playerSelection} is not a valid option, please try again`;
-    if (((playerSelection == 'rock') && (computerSelection == 'scissors')) || 
-    (playerSelection == 'paper') && (computerSelection == 'rock')||
-    (playerSelection == 'scissors')&&(computerSelection == 'paper')){
-        return winMessage;
-    }
-    else if (((playerSelection == 'rock') && (computerSelection == 'paper')) || 
-    (playerSelection == 'paper') && (computerSelection == 'scissors') ||
-    (playerSelection == 'scissors')&& (computerSelection == 'rock')){
-        return loseMessage;
-    }
-    else if ((playerSelection == computerSelection)){
-        return tieMessage;
-    }
-    else { return naMessage};
-}
-
-*/
-
-
-
-
-function game(){
-    let playerScore = 0;
-    let computerScore = 0;
-    let scoreboard = `Player Score: ${playerScore}  Computer Score: ${computerScore}`;
-
-    for(let i=0; i<5; i++){
-        let result = playRound();
-        if (result == winMessage) {
-            console.log(winMessage);
-            playerScore +=1;
-            scoreboard = `Player Score: ${playerScore}  Computer Score: ${computerScore}`;
-            console.log(scoreboard);
-        }
-        else if (result == loseMessage){
-            console.log(loseMessage) ;
-            computerScore +=1;
-            scoreboard = `Player Score: ${playerScore}  Computer Score: ${computerScore}`;
-            console.log(scoreboard);
-        }
-        else{
-            console.log(tieMessage) ;
-            console.log(scoreboard);
-        }
-    }
-   
-}
-
-function printing(){for (let i=0;i<5;i++){
-    console.log(i)
-}
-}
